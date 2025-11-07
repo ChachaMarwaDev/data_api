c2c08b5011afaea19bc78c6227feb99596bb6599
Chacha Marwa <chachamarwa003@gmail.com>
2025-11-07 04:06:16 +0300

    Refactor inspecting data

--- csv_loading/insurance_preparation.py
+++ csv_loading/insurance_preparation.py
@@ -11,22 +11,11 @@ from pathlib import Path
 path = Path(r"csv_files\insurance.csv")
 df = pd.read_csv(path)
 
-print(df.columns)
-
-print(df['age'])
-# know the types of data in our csv file
-# dataTypes = print(df.dtypes)
-
-# First5 = df.head(5)
-# print(First5)
-
-# last5 = df.tail(5)
-# print(last5)
-
+#shows column names in dataframe
 # print(df.columns)
-# 'age', 'sex', 'bmi', 'children', 'smoker', 'region', 'charges'
-'''
-how do we select a one column?
-how do we select two columns?
-'''
 
+# gives a statistical summary of numerical columns
+# print(df.info()) 
+
+
+# print(df.describe())
