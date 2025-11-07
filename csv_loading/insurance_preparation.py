87152787ab8ce3099cb24dbf01e719a99bd6a3b5
Chacha Marwa <chachamarwa003@gmail.com>
2025-11-07 05:53:09 +0300

    Refactor renaming and filling data

--- csv_loading/insurance_preparation.py
+++ csv_loading/insurance_preparation.py
@@ -8,14 +8,45 @@ from pathlib import Path
 
 # if __name__ == "__main__":
 #     main()
+
+# Data loading
 path = Path(r"csv_files\insurance.csv")
 df = pd.read_csv(path)
+# making a temporary copy of the file
+df_copy = df.copy()
+
+#Data inspection
 
 #shows column names in dataframe
-# print(df.columns)
+# print(df.columns) # these are columns:['age', 'sex', 'bmi', 'children', 'smoker', 'region', 'charges']
 
+# checking the data types 
+# print(df_copy.dtypes)
 # gives a statistical summary of numerical columns
-# print(df.info()) 
+# print(df_copy.info()) 
+
+# see statistical summary
+# print(df_copy.describe())
+
+# Checks a no. of missing values in each column
+# print(df_copy.isnull().sum()) # columns age,bmi,children have 1 missing value
+
+# check fro duplicate rows
+# print(df_copy.duplicated().sum()) # there is 1 duplicate row
+
+# check no. of unique values
+# print(df_copy.nunique()) # age:47, sex:2, bmi:548, children:6, smoker:2, region:4, charges:1337
 
 
-# print(df.describe())
+df_copy.fillna(df_copy['age'].median(), inplace=True)
+df_copy.fillna(df_copy['bmi'].median(), inplace=True)
+df_copy.fillna(df_copy['children'].median(), inplace=True)
+
+
+df_copy['children'] = df_copy['children'].astype(int)
+df_copy['age'] = df_copy['age'].astype(int)
+print(df_copy['children'].head(5))
+
+
+
+# print(df_copy.info()) 
