8e76fadd7299b333e1983a732f758a36de3b5687
Chacha Marwa <chachamarwa003@gmail.com>
2025-11-14 12:14:05 +0300

    Refactor: removed a row in .csv file to improve integrity, hance creating a new database.

--- csv_loading/check_db.py
+++ csv_loading/check_db.py
@@ -15,17 +15,23 @@ def run_query(query):
 # --------------------------------------------
 
 # Check all tables
-tables = run_query("SELECT name FROM sqlite_master WHERE type='table';")
-print("Tables:", tables)
+# tables = run_query("SELECT name FROM sqlite_master WHERE type='table';")
+# print("Tables:", tables)
 
-# Preview your data
-rows = run_query("SELECT * FROM insurance LIMIT 5;")
-print("Sample rows:", rows)
+# # Preview your data
+# rows = run_query("SELECT * FROM insurance LIMIT 5;")
+# print("Sample rows:", rows)
 
-print(run_query("SELECT COUNT(*) FROM insurance;"))
+# print(run_query("SELECT COUNT(*) FROM insurance;"))
 
-print(run_query("SELECT age, charges " \
-                "FROM insurance " \
-                "WHERE age > 50 " \
-                "ORDER BY age DESC " \
-                "LIMIT 10 ;"))
+# print(run_query("SELECT age, charges " \
+#                 "FROM insurance " \
+#                 "WHERE age > 50 " \
+#                 "ORDER BY age DESC " \
+#                 "LIMIT 10 ;"))
+
+print(run_query("SELECT DISTINCT children FROM insurance;"))
+
+'''
+age, sex, bmi, smoker, region, charges 
+'''
